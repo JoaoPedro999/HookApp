4119116aa359dad809aadb614a8d0d984242560a
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-03-22 12:12:54 -0300

    HookApp

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, TextInput, Modal } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  const changeTxt = (name) => {
+    setName(`Olá ${name}, bom dia. Tudo bom?`);
+  };
+
+  useEffect(() => {
+    if (name.length === 10) {
+      window.alert("A string atingiu 10 caracteres de comprimento!");
+    }
+  }, [name]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Contador: {count}</Text>
+      <Button title="Increment" onPress={incrementCount} />
+      <Button title="Decrement" onPress={decrementCount} />
+      <TextInput
+        placeholder="Digite seu nome"
+        style={{
+          width: 300,
+          padding: 10,
+          borderWidth: 1,
+          borderColor: "black",
+        }}
+        onChangeText={changeTxt}
+      />
+      <Text>{name}</Text>
     </View>
   );
 }
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
